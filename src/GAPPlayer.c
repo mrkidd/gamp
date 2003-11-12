b9b924b4995e9a77210d84e4fb6a5a61c1568e2d
Mason Kidd <mrkidd@mrkidd.com>
2003-11-12 04:10:26 +0000

    Update source files with copyright message.

--- src/GAPPlayer.c
+++ src/GAPPlayer.c
@@ -1,9 +1,21 @@
 /*
- * File: GAPPlayer.c
- * Created by: Mason Kidd <mrkidd@mrkidd.com>
- * Created on: Fri Oct 10 14:52:43 2003
+ *  GAPPlayer object implementation
  *
- * Gnome Audio Player object implementation
+ *  Copyright (C) 2003 Mason Kidd <mrkidd@mrkidd.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
  */
 #include <gst/gst.h>
